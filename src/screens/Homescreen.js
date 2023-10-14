96bdd37fede2fb4f5e838415a129234b34ce8393
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-10-14 20:08:09 +0530

    new file added

---
+++ src/screens/Homescreen.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Menusa from "../components/Menus";
+import { getAllitems } from "../actions/MenuActions";
+import Loading from "../components/Loading";
+import Error from "../components/Error";
+import { Row } from "react-bootstrap";
+import Filters from "../components/Filters";
+import Slidebar from "../components/Slidebar";
+
+export default function Homescreen() {
+  const dispatch = useDispatch();
+
+  const itemstate = useSelector((state) => state.getAllitemsReducer);
+
+  const { items, error, loading } = itemstate;
+  useEffect(() => {
+    if (localStorage.getItem("currentUser") === null) {
+      window.location.href = "/";
+    }
+  }, []);
+  useEffect(() => {
+    dispatch(getAllitems());
+  }, []);
+  return (
+   <div>
+    <Slidebar/>
+    <Filters/>
+     <div className="democ">
+      {loading ? (
+        <Loading/>
+      ) : error ? (
+        <Error error='something went wrong'/>
+      ) : (
+        items.map((menu) => {
+          return (
+            <div>
+              <div style={{ margin: "-8px" }} key={menu._id}>
+                <Menusa menu={menu} />
+              </div>
+            </div>
+          );
+        })
+      )}
+    </div>
+   </div>
+  );
+}
