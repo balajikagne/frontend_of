8c7a84bd1d0194e06825eb33a8d2e6877601afd9
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-10-16 15:28:15 +0530

    Update cartScreen.js new statement
    new statement

--- src/screens/cartScreen.js
+++ src/screens/cartScreen.js
@@ -22,6 +22,7 @@ return (
         <div>
             <h2 style={{fontSize:'40px'}}>My Cart</h2><br></br>
              <h2 style={{fontSize:'40px'}}>(Free Delivery Above 100 RS /-)</h2>
+            <h2 style={{fontSize:'40px'}}>(30 RS /- Delivery Charges Below 100 Rs /-)</h2>
             <div className='totalp' style={{display:'flex'}}>
             <h2 style={{fontSize:'40px'}}>Subtotal : {subtotal} /Rs-</h2>
             <button className="btn" style={{height:'40px',marginLeft:'50px'}}><NavLink to={"/checkout"} style={{fontSize:'20px',textDecoration:'none',cursor:"pointer"}}>Order Now</NavLink></button>
