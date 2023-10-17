b52b4cbc3a8623af10875a424756d8b8076d56e3
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-10-17 22:19:51 +0530

    Update Slidebar.js new changes in post
    post is changed

--- src/components/Slidebar.js
+++ src/components/Slidebar.js
@@ -6,19 +6,19 @@ export default function Slidebar() {
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="pro1.jpg" alt="First slide"></img>
+      <img class="d-block w-100" src="pro1.jpg" </img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="pro2.png" alt="Second slide"></img>
+      <img class="d-block w-100" src="pro2.png" </img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="pro3.png" alt="Third slide"></img>
+      <img class="d-block w-100" src="pro3.png" </img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="pro4.png" alt="Third slide"></img>
+      <img class="d-block w-100" src="pro4.png" </img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="p5.png" alt="Third slide"></img>
+      <img class="d-block w-100" src="p5.png" </img>
     </div>
   </div>
 </div>
