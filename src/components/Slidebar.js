8a837e91aa463593bed9d05e0d60195c56e358ba
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-11-11 00:34:50 +0530

    Update Slidebar.js slideshow

--- src/components/Slidebar.js
+++ src/components/Slidebar.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import "react-bootstrap";
-import "react-slideshow-image";
 export default function Slidebar() {
   return (
     <div style={{marginTop:'100px'}}>
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="https://scontent.fpnq13-3.fna.fbcdn.net/v/t39.30808-6/396725278_122134860992025132_5596091832507754600_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=OCzpYIMUURgAX8RYu5I&_nc_ht=scontent.fpnq13-3.fna&oh=00_AfBFxZ_B2phw52fAIjNJrjPlboaf1eMSGzHrqUlKLSc-OQ&oe=654593EF" ></img>
+      <img class="d-block w-100" src="pro1.jpg" alt="First slide"></img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="pro2.png" ></img>
+      <img class="d-block w-100" src="pro2.png" alt="Second slide"></img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="pro3.png" ></img>
+      <img class="d-block w-100" src="pro3.png" alt="Third slide"></img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="pro4.png" ></img>
+      <img class="d-block w-100" src="pro4.png" alt="Third slide"></img>
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="p5.png" ></img>
+      <img class="d-block w-100" src="p5.png" alt="Third slide"></img>
     </div>
   </div>
 </div>
