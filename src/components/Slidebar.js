9febdfd8159e1b2e8e810adb277746a6af8f51ab
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-11-24 01:18:03 +0530

    Update Slidebar.js new slide show is designed

--- src/components/Slidebar.js
+++ src/components/Slidebar.js
@@ -6,6 +6,7 @@ export default function Slidebar() {
     <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
+      <img class="d-block w-100" src="pro1.jpg" alt="First slide"></img>
     </div>
     <div class="carousel-item">
       <img class="d-block w-100" src="pro2.png" alt="Second slide"></img>
