0bf74401122d1b29a40ebf8e0ef394f272d8a16d
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-12-07 22:59:49 +0530

    front ent of main

--- src/screens/OrderScreen.js
+++ src/screens/OrderScreen.js
@@ -5,6 +5,9 @@ import Success from "../components/Success";
 import Loading from "../components/Loading";
 import Error from "../components/Error";
 import { getUserOrderReducer } from '../Reducers/orderReducer'
+import { NavLink } from 'react-router-dom';
+import FullOrderInfo from './FullOrderInfo';
+import MapOrders from './MapOrders';
 // import { getUserOrders } from '../actions/OrderActions';
 export default function OrderScreen() {
     const dispatch=useDispatch();
@@ -14,39 +17,23 @@ export default function OrderScreen() {
         dispatch(getUserOrders())
     },[])
   return (
-    <div>
-      <h2 style={{fontSize:'35px'}}>My orders</h2>
+    <>
+    
       <div className='row'>
+      <hr></hr>
       {loading && (<Loading/>)}
         {error && (<Error error="something went wrong"/>)}
         {orders && orders.map((order)=>{
-           return <div className='col-md-8'>
-            <div className='flex-container'>
-                <div className='text-left w-100 m-1'>
-                    <h2 style={{fontSize:'25px'}}>Items</h2>
-                    {order.orderItems.map(item=>{
-                        return <div>
-                            <h1>{item.name} [{item.varient}]*{item.quantity}*{item.price}</h1>
-                        </div>
-                    })}
-                </div>
-                <div className='text-left w-100 m-1'>
-                    <h2 style={{fontSize:'25px'}}>Address</h2>
-                    <h1> street :{order.shippingAddress.street}</h1>
-                    <h1> city :{order.shippingAddress.city}</h1>
-                    {/* <h1> country :{order.shippingAddress.country}</h1> */}
-                    <h1> pincode :{order.shippingAddress.pincode}</h1>
-                </div>
-                <div style={{display:'flex',gap:'10px'}}>
-                    <h2>Total Amount :{order.orderAmount}</h2>
-                    <h2>Data :{order.createdAt.substring(0,10)}</h2>
-                </div>
-                </div>
-                <br></br>
-                <hr></hr>
-           </div>
+           return (
+            <div>
+              <div style={{ margin: "-8px" }}>
+                <MapOrders order={order}/>
+              </div>
+            </div>
+          );
         })}
       </div>
-    </div>
+      
+    </>
   )
 }
