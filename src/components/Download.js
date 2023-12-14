71e471c0fe85f3f79abddac87564921d90985c8e
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-12-14 15:51:21 +0530

    Update Download.js dfgdf

--- src/components/Download.js
+++ src/components/Download.js
@@ -1,6 +1,17 @@
 import React from "react";
 import application from './testygo.apk'
+import addNotification from "react-push-notification";
 export default function Download() {
+  function notificationcall(){
+      console.log('hellow')
+      addNotification({
+        title:'Order confirmed',
+        message:'your order',
+        duration:4000,
+        // icon:logo,
+        native:true,
+      })
+    }
   return (
     <>
      <div style={{display:'inline',justifyContent:'center',alignItems:'center'}}>
@@ -9,7 +20,7 @@ export default function Download() {
             <img src="logo1.png" height={'100px'}></img>
         <h3 style={{fontSize:'40px'}}>TestyGo</h3>
   <li className="nav-item">
-              <a className="dbtn" href={application} download='testygo.apk' target="_blank" rel="noreferrer" >
+              <a className="dbtn" href={application} download='testygo.apk' target="_blank" rel="noreferrer" onClick={notificationcall}>
                 <i class="fa-solid fa-download" style={{fontSize:'40px',paddingRight:'10px'}}></i>Download App
               </a>
             </li>
