2fddf833227912a8bf3e7f0c8dd8a50561cc275d
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-12-16 22:04:16 +0530

    helpline number i s added

---
+++ src/screens/HelpLine.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function HelpLine() {
+  return (
+    <>
+       <>
+     <div style={{display:'inline',justifyContent:'center',alignItems:'center'}}>
+        <h4 style={{fontSize:'40px'}}>HelpLine Number</h4><br></br>
+        <p>📞 TestyGo Helpline: Taste the Convenience
+Embark on a journey of gastronomic delight with TestyGo, your trusted food delivery platform. Our helpline is your personal guide through the vast array of delectable options available on our platform. Whether you have questions about our diverse menu selections, need assistance with placing an order, or require support with special dietary requests, our knowledgeable and friendly customer service representatives are ready to assist you.</p>
+        <img src="logo1.png" height={'100px'}></img>
+        <h3 style={{fontSize:'40px'}}>TestyGo</h3>
+        <div>
+        <li className="nav-item">
+        <a href="tel:+919309100534" style={{color:'black',textDecoration:'none',color:'green'}}><i class="fa-solid fa-phone" style={{fontSize:'20px',paddingRight:'10px'}}></i>Helpline Number-1</a>
+            </li>
+            <li className="nav-item">
+        <a href="tel:+917498821001" style={{color:'black',textDecoration:'none',color:'green'}}><i class="fa-solid fa-phone" style={{fontSize:'20px',paddingRight:'10px'}}></i>Helpline Number-2</a>
+            </li>
+        </div>
+     </div>
+    </>
+    </>
+  )
+}
+
+export default HelpLine
