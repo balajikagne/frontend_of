11e3148a84c6af238ccd71c00a8077e338fb0de0
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-12-17 21:35:01 +0530

    Update Homescreen.js router

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -25,10 +25,10 @@ export default function Homescreen() {
       }).then((result) => {
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-          window.location.href='/login'
+          window.location.href='/'
         }
         else{
-          window.location.href='/login'
+          window.location.href='/'
         }
       })
     }
