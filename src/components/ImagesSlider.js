a21cf455706dcead16968768a7a7386a64fa4f8a
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-12-19 03:40:11 +0530

    Update ImagesSlider.js dggee

--- src/components/ImagesSlider.js
+++ src/components/ImagesSlider.js
@@ -1,25 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect ,useRef} from "react";
 import { slideImages } from "./Imagses";
 import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from "react-icons/fa";
 
 export default function ImagesSlider({ slides }) {
   const [current, setCurrent] = useState(0);
   const length = slides.length;
-
+  // const videoRef=useRef()
   const nextSlide = () => {
     setCurrent(current === length - 1 ? 0 : current + 1);
+    // videoRef.current.play();
   };
 
   const prevSlide = () => {
     setCurrent(current === 0 ? length - 1 : current - 1);
+    // videoRef.current.play();
   };
 
   useEffect(() => {
     // Function to transition to the next slide every 10 seconds
     const intervalId = setInterval(() => {
       nextSlide();
+      
     }, 10000);
-
+    
     // Cleanup function to clear the interval when the component unmounts
     return () => clearInterval(intervalId);
   }, [current, length]);
@@ -27,7 +30,9 @@ export default function ImagesSlider({ slides }) {
   if (!Array.isArray(slides) || slides.length <= 0) {
     return null;
   }
-
+const handle=()=>{
+  
+}
   return (
     <>
       <section className="slider">
@@ -39,10 +44,18 @@ export default function ImagesSlider({ slides }) {
             key={index}
           >
             {index === current && (
-              <img src={slide.images} alt="travel images" className="Imageslide" />
+              // <img src={slide.images} alt="travel images" className="Imageslide" />
+              <video className="videoplayer" muted autoPlay>
+        <source
+          src={slide.images}
+          type="video/mp4"
+        />
+        Your browser does not support the video tag.
+      </video>
             )}
           </div>
         ))}
+       
       </section>
     </>
   );
