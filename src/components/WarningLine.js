6111edf0ce6974e74b806ed25cbf7a0441df13fb
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2023-12-26 18:43:51 +0530

    warning

---
+++ src/components/WarningLine.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function WarningLine() {
+  return (
+    <>
+      <div className='redline'>
+        <div className='headline'><h2 >something went wrong</h2></div>
+      </div>
+    </>
+  )
+}
+
+export default WarningLine
