1dde13a3cb3f6e0fe3683f8f6c3a294921eede98
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-01-18 07:07:14 +0530

    Update Checkout.js new changes are maded

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -250,10 +250,8 @@ const Checkout=({subtotal})=> {
               icon: "success",
         confirmButtonText: "OK",
       }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-          window.location.href='/'
-        }
+          window.location.href='/orders'
+        
       });
       }
     };
