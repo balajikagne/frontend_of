a754261329cebb41e4e1917a7b72d25780beb05d
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-01-18 10:09:10 +0530

    ffdg

---
+++ src/screens/NotifyScreen.js
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getAllitems, getAllitems_new } from '../actions/MenuActions'
+import Loading from '../components/Loading'
+import Error from '../components/Error'
+import NotifyMap from '../components/NotifyMap'
+
+function NotifyScreen() {
+    const dispatch=useDispatch()
+    
+    const notification=useSelector(notify=>notify.setAllitemsReducer)
+    const { items, error, loading }=notification;
+    console.log(notification)
+    useEffect(()=>{
+      dispatch(getAllitems_new())
+      dispatch(getAllitems())
+  },[])
+  return (
+    <>
+    {loading ? (
+        <Loading/>
+      ) : error ? (
+        <Error error='something went wrong'/>
+      ) : (
+        items.map((notifyME) => {
+          return (
+            <div key={notifyME._id}><NotifyMap notifyME={notifyME}/><hr></hr></div>
+          );
+        })
+      )}
+    </>
+  )
+}
+
+export default NotifyScreen
