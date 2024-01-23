db3c4e8d17ef4ebd91948353b692d1538b9484e7
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-01-23 14:11:33 +0530

    Update Imagses.js hhj

--- src/components/Imagses.js
+++ src/components/Imagses.js
@@ -1,6 +1,6 @@
 import prop1 from "./prop-1.json"
 import prop2 from "./prop-2.json"
-import prop3 from "./IMG_20240123_WA0030.json"
+import prop3 from "./IMG-20240123-WA00311705998991.json"
 export const slideImages =[
     {
         images:prop1
