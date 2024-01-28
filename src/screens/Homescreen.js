b0c72cb585c091e9bfc12509857f072e037d884b
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-01-28 13:09:41 +0530

    Update Homescreen.js new changes has been done recently

--- src/screens/Homescreen.js
+++ src/screens/Homescreen.js
@@ -8,38 +8,46 @@ import { Row } from "react-bootstrap";
 import Filters from "../components/Filters";
 import Slidebar from "../components/Slidebar";
 import swal from "sweetalert2";
+
 export default function Homescreen() {
   const dispatch = useDispatch();
-
   const itemstate = useSelector((state) => state.getAllitemsReducer);
-
   const { items, error, loading } = itemstate;
-  
+
   useEffect(() => {
     dispatch(getAllitems());
   }, []);
+
   return (
-   <div>
-   
-    <Slidebar/>
-    <Filters/>
-     <div className="democ">
-      {loading ? (
-        <Loading/>
-      ) : error ? (
-        <Error error='We have been working on Testygo app for two days'/>
-      ) : (
-        items.map((menu) => {
-          return (
-            <div>
-              <div style={{ margin: "-8px" }} >
-                <Menusa menu={menu} key={menu._id}/>
+    <div>
+      <Slidebar />
+      <Filters />
+      <div className="democ">
+        {loading ? (
+          <Loading />
+        ) : error ? (
+          <Error error="We have been working on Testygo app for two days" />
+        ) : (
+          items
+            .slice()
+            .sort((a, b) => {
+              // First, sort by availability (onlineAvailable)
+              if (a.stock && !b.stock) {
+                return -1; // 'a' is available, 'b' is not, so 'a' comes first
+              } else if (!a.stock && b.stock) {
+                return 1; // 'a' is not available, 'b' is, so 'b' comes first
+              }
+
+              // Within each group, sort by updatedAt in descending order
+              return new Date(b.updatedAt) - new Date(a.updatedAt);
+            })
+            .map((menu) => (
+              <div key={menu._id} style={{ margin: "-8px" }}>
+                <Menusa menu={menu} />
               </div>
-            </div>
-          );
-        })
-      )}
+            ))
+        )}
+      </div>
     </div>
-   </div>
   );
 }
