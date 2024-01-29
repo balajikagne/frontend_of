3caa496f29c9eb73a1f5c3590fde9e21952e23e4
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-01-29 14:45:56 +0530

    Update Imagses.js sultan

--- src/components/Imagses.js
+++ src/components/Imagses.js
@@ -1,6 +1,5 @@
 import prop1 from "./prop-1.json"
 import prop2 from "./prop-2.json"
-import prop3 from "./IMG-20240123-WA00311705998991.json"
 export const slideImages =[
     {
         images:prop1
