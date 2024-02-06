3875b3d3e3c2384299be77f08d93c62f24024e6d
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-02-06 13:29:27 +0530

    Update Checkout.js lkk

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -260,7 +260,6 @@ const Checkout=({subtotal})=> {
             let checker=false;
             dispatch(placeOrder(item,prizeValue));
              setDemo(false)
-            dispatch(resetCart());
            
           }
           else{
@@ -277,7 +276,6 @@ const Checkout=({subtotal})=> {
             let checker=false;
              dispatch(placeOrder(item,prizeValue));
             setDemo(false)
-            dispatch(resetCart());
             
           }
           else{
