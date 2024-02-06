fed625cf05dbc767ee4de63669019bc3a4b0f29d
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-02-06 07:23:40 +0530

    Update cartReducer.js cbgfh

--- src/Reducers/cartReducer.js
+++ src/Reducers/cartReducer.js
@@ -1,32 +1,35 @@
-export const addtoCartReducer=(state={cartItems:[]},action)=>{
-
-    try{
-        switch (action.type)
-    {
+export const addtoCartReducer = (state = { cartItems: [] }, action) => {
+    try {
+      switch (action.type) {
         case 'ADD_TO_CART':
-            const alradyExists=state.cartItems.find(item=>item._id===action.payload._id)
-            if (alradyExists){
-                return {
-                    ...state,
-                    cartItems:state.cartItems.map(item=>item._id===action.payload._id ? action.payload :item)
-                }
-            }
-            else{
-                return{
-                    ...state,
-                    cartItems:[...state.cartItems,action.payload]
-                }
-
-            }
-            case 'DELETE_FROM_CART':return{
-                ...state,
-                cartItems :state.cartItems.filter(item=>item._id!==action.payload._id)
-            }
-            
-        default:return state
+          const alreadyExists = state.cartItems.find(item => item._id === action.payload._id);
+          if (alreadyExists) {
+            return {
+              ...state,
+              cartItems: state.cartItems.map(item => (item._id === action.payload._id ? action.payload : item)),
+            };
+          } else {
+            return {
+              ...state,
+              cartItems: [...state.cartItems, action.payload],
+            };
+          }
+        case 'DELETE_FROM_CART':
+          return {
+            ...state,
+            cartItems: state.cartItems.filter(item => item._id !== action.payload._id),
+          };
+        case 'RESET_CART':
+          return {
+            ...state,
+            cartItems: [],
+          };
+  
+        default:
+          return state;
+      }
+    } catch (error) {
+      console.log(error);
     }
-    }
-    catch(error){
-        console.log(error)
-    }
-}
+  };
+  
