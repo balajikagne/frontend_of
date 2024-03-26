64583e7059de3a311bbf04555c775f6fc493b7b3
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-03-26 16:14:43 +0530

    Update UserActions.js dvjfdgkfnjdv

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 import swal from 'sweetalert2'
-export const registerUser=(user)=>async dispatch=>{
-    dispatch({type:'USER_REGISTER_REQ'})
-    try{
-        const response=await axios.post('https://zany-clam-sundress.cyclic.app/api/users/register',user)
-        
-        dispatch({type:'USER_REGISTER_SUCCESS'})
+export const registerUser = (user) => async (dispatch) => {
+    dispatch({ type: 'USER_REGISTER_REQ' });
+
+    try {
+        const response = await axios.post('https://balajibackend-demo-1.onrender.com/api/users/register', user);
+        dispatch({ type: 'USER_REGISTER_SUCCESS' });
+        const response2 = await axios.post('https://balajibackend-demo-1.onrender.com/api/users/login', user);
+        dispatch({ type: 'USER_LOGIN_SUCCESS', payload: response2.data });
+        localStorage.setItem('currentUser', JSON.stringify(response2.data));
+        window.location.href = '/';
+    } catch (error) {
+        dispatch({ type: 'USER_REGISTER_FAILED', payload: error.message });
+        // Consider handling different types of errors more specifically
     }
-    catch(error){
-        dispatch({type:'USER_REGISTER_FAILED',payload:error})
-    }
-}
+};
 
 export const loginUser=(user)=>async dispatch=>{
     dispatch({type:'USER_LOGIN_REQ'})
