4e0383ed9eb6cc7c1ff9d253073ef552c7531458
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2024-05-14 21:04:06 +0530

    Update UserActions.js DFFW

--- src/actions/UserActions.js
+++ src/actions/UserActions.js
@@ -4,9 +4,9 @@ export const registerUser = (user) => async (dispatch) => {
     dispatch({ type: 'USER_REGISTER_REQ' });
 
     try {
-        const response = await axios.post('https://balajibackend-demo-1.onrender.com/api/users/register', user);
+        const response = await axios.post('https://gifted-apron-toad.cyclic.app/api/users/register', user);
         dispatch({ type: 'USER_REGISTER_SUCCESS' });
-        const response2 = await axios.post('https://balajibackend-demo-1.onrender.com/api/users/login', user);
+        const response2 = await axios.post('https://gifted-apron-toad.cyclic.app/api/users/login', user);
         dispatch({ type: 'USER_LOGIN_SUCCESS', payload: response2.data });
         localStorage.setItem('currentUser', JSON.stringify(response2.data));
         window.location.href = '/';
@@ -20,7 +20,7 @@ export const loginUser=(user)=>async dispatch=>{
     dispatch({type:'USER_LOGIN_REQ'})
 
     try{
-        const response=await axios.post('https://zany-clam-sundress.cyclic.app/api/users/login',user)
+        const response=await axios.post('https://gifted-apron-toad.cyclic.app/api/users/login',user)
         // console.log(response)
         dispatch({type:'USER_LOGIN_SUCCESS',payload: response.data})
         localStorage.setItem('currentUser',JSON.stringify(response.data))
